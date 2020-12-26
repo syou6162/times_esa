591f07044069842f081bcf760ea93de223f31f70
Yasuhisa Yoshida <syou6162@gmail.com>
2020-12-27 02:14:11 +0900

    lintかけた

---
+++ src/App.tsx
@@ -0,0 +1,113 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+// firebase functions
+import firebase from 'firebase';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import {
+  Button, TextField, Container,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { firebaseAuth } from './firebase/index';
+
+const useStyles = makeStyles(() => ({
+  multilineColor: {
+    color: 'white',
+  },
+  notchedOutline: {
+    borderWidth: '1px',
+    margin: '10px',
+    borderColor: 'white',
+  },
+}));
+
+function App() {
+  const classes = useStyles();
+
+  const [loading, setLoading] = useState(true);
+  const [text, setText] = useState<string>('');
+  const [esaText, setEsaText] = useState<string>('');
+  const [myAccount, setMyAccount] = useState<firebase.User>();
+
+  const uiConfig = {
+    signInFlow: 'popup',
+    signInSuccessUrl: '/',
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    ],
+  };
+
+  const submitTextToEsa = async () => {
+    const helloWorld = firebase.functions().httpsCallable('helloWorld');
+    const data = await helloWorld({ text });
+    setText('');
+    setEsaText(data.data.body_md);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    firebaseAuth.onAuthStateChanged((user) => {
+      setLoading(false);
+      if (!user) return;
+      if (user.email !== process.env.REACT_APP_VALID_MAIL_ADDRESSES) return;
+      setMyAccount(user);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        { /* eslint no-nested-ternary: 0 */ }
+        {loading ? (
+          <p>
+            LOADING.....
+          </p>
+        ) : !myAccount ? (
+          <div>
+            ログインが必要です
+            <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+          </div>
+        )
+          : (
+            <Container maxWidth="xl">
+              #times_esa
+              <form>
+                <TextField
+                  fullWidth
+                  multiline
+                  placeholder="ここにつぶやいた内容がesa.ioに追記されていきます"
+                  variant="outlined"
+                  InputProps={{
+                    classes: {
+                      root: classes.multilineColor,
+                      notchedOutline: classes.notchedOutline,
+                    },
+                  }}
+                  rows={10}
+                  value={text}
+                  onChange={(event) => { setText(event.target.value); }}
+                />
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={() => submitTextToEsa()}
+                >
+                  つぶやく
+                </Button>
+
+              </form>
+              <div style={{
+                whiteSpace: 'pre-wrap', textAlign: 'left', justifyContent: 'left', alignItems: 'left',
+              }}
+              >
+                {esaText}
+              </div>
+            </Container>
+
+          )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
