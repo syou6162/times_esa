328c5759f1ce028e402edeeef523f99154991378
Yasuhisa Yoshida <syou6162@gmail.com>
2021-01-01 18:31:15 +0900

    テストしやすいように設定切り出す

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,34 @@
 import * as functions from 'firebase-functions';
+import { AxiosInstance } from 'axios'
 
-const axiosBase = require('axios');
+type EsaConfig = {
+  teamName: string;
+  accessToken: string;
+}
 
-const teamName = functions.config().esa.team_name;
-const axios = axiosBase.create({
-  baseURL: 'https://api.esa.io',
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${functions.config().esa.access_token}`,
-  },
-  responseType: 'json',
-});
+function getEsaConfig(): EsaConfig {
+  const teamName = functions.config().esa.team_name;
+  const accessToken = functions.config().esa.access_token;
+  const config: EsaConfig = { teamName, accessToken }
+  return config;
+};
 
-async function createOrUpdatePost(category: string, title: string, text: string) {
-  const res = await axios.get(`/v1/teams/${teamName}/posts`, {
+function createAxiosClient(accessToken: string): AxiosInstance {
+  const axiosBase = require('axios');
+
+  const axios = axiosBase.create({
+    baseURL: 'https://api.esa.io',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+    responseType: 'json',
+  });
+  return axios;
+}
+
+async function createOrUpdatePost(axios: AxiosInstance, esaConfig: EsaConfig, category: string, title: string, text: string) {
+  const res = await axios.get(`/v1/teams/${esaConfig.teamName}/posts`, {
     params: {
       q: `category:${category} title:${title}`,
     },
@@ -27,47 +42,55 @@ async function createOrUpdatePost(category: string, title: string, text: string)
         body_md: text,
         wip: false,
       },
+    }).then((res) => {
+      return res.data;
     });
   }
   functions.logger.info('記事があったよ');
-  return axios.patch(`/v1/teams/${teamName}/posts/${res.data.posts[0].number}`, {
+  return axios.patch(`/v1/teams/${esaConfig.teamName}/posts/${res.data.posts[0].number}`, {
     post: {
       name: title,
       category,
       body_md: `${text}\n${res.data.posts[0].body_md}`,
       wip: false,
     },
+  }).then((res) => {
+    return res.data;
   });
 }
 
-async function getDailyReport(category: string, title: string) {
+async function getDailyReport(axios: AxiosInstance, esaConfig: EsaConfig, category: string, title: string) {
   functions.logger.info(title);
-  const res = await axios.get(`/v1/teams/${teamName}/posts`, {
+  const res = await axios.get(`/v1/teams/${esaConfig.teamName}/posts`, {
     params: {
       q: `category:${category} title:${title}`,
     },
   });
   if (res.data.total_count === 0) {
     functions.logger.info('記事がなかったよ!');
-    return { body_md: '' };
+    return { body_md: '', body_html: '' };
   }
   functions.logger.info('記事があったよ');
-  return axios.get(`/v1/teams/${teamName}/posts/${res.data.posts[0].number}`);
+  return axios.get(`/v1/teams/${esaConfig.teamName}/posts/${res.data.posts[0].number}`).then((res) => res.data);
 }
 
 export const submitTextToEsa = functions.https.onCall(async (req) => {
   functions.logger.info('Hello logs!', req);
-  const tmp = await createOrUpdatePost(req.category, req.title, req.text);
-  functions.logger.info('returned json', tmp.data);
-  return tmp.data;
+  const esaConfig = getEsaConfig();
+  const axios = createAxiosClient(esaConfig.accessToken);
+  const result = await createOrUpdatePost(axios, esaConfig, req.category, req.title, req.text);
+  functions.logger.info('returned json', result);
+  return result;
 });
 
 // あとでonCallに変える
 // 引数二つ
 export const dailyReport = functions.https.onCall(async (req) => {
   functions.logger.info('Hello logs!', req);
+  const esaConfig = getEsaConfig();
+  const axios = createAxiosClient(esaConfig.accessToken);
 
-  const tmp = await getDailyReport(req.category, req.title);
-  functions.logger.info('returned json', tmp.data);
-  return tmp.data;
+  const result = await getDailyReport(axios, esaConfig, req.category, req.title);
+  functions.logger.info('returned json', result);
+  return result;
 });
