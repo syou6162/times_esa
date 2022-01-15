551b5557a32d9fe7e88cb78a44b7eace5fb63fb2
Yasuhisa Yoshida <syou6162@gmail.com>
2022-01-15 17:53:27 +0900

    送信中は入力できないことのテスト

--- src/components/EsaTagsField/index.spec.tsx
+++ src/components/EsaTagsField/index.spec.tsx
@@ -1,4 +1,5 @@
 import { render } from '@testing-library/react'
+import '@testing-library/jest-dom'
 import { EsaTagsField, EsaTagsFieldProps } from '.'
 
 describe('esaのタグが正しく表示される', () => {
@@ -17,4 +18,19 @@ describe('esaのタグが正しく表示される', () => {
     expect(renderResult.asFragment()).toMatchSnapshot();
   });
 
+  it('送信中は入力できない', async () => {
+    const props: EsaTagsFieldProps = {
+      sending: true,
+      fetching: false,
+      tags: [],
+      tagCandidates: [],
+      onChange: () => { }
+    }
+    const renderResult = render(
+      <EsaTagsField {...props} />
+    );
+    
+    expect(renderResult.getByRole("textbox")).toBeDisabled();
+    expect(renderResult.asFragment()).toMatchSnapshot();
+  });
 })
