8aba92031d0cccc571dbd78ddea12d89247d1cff
Yasuhisa Yoshida <syou6162@gmail.com>
2022-05-08 06:09:19 +0900

    ボタンを押した際の挙動をテスト

--- src/components/DailyReport/index.spec.tsx
+++ src/components/DailyReport/index.spec.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react'
+import { fireEvent, render, waitFor } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import { DailyReport, DailyReportProps } from '.'
 
@@ -8,8 +8,8 @@ describe('DailyReportが正しく表示される', () => {
       fetching: false,
       fetchErrorMessage: "",
 
-      esaText: "hello world",
-      esaHtml: "<span>hello world<span>",
+      esaText: "hello world!",
+      esaHtml: "<span>HTML TEXT<span>",
       reloadDailyReport: () => { }
     }
     const renderResult = render(
@@ -17,5 +17,28 @@ describe('DailyReportが正しく表示される', () => {
     );
 
     expect(renderResult.asFragment()).toMatchSnapshot();
+    expect(renderResult.getByText("HTML TEXT")).toBeTruthy();
+    expect(renderResult.queryByText("hello world")).toBeNull();
+  });
+
+  it('textをクリックするとtextが表示される', async () => {
+    const props: DailyReportProps = {
+      fetching: false,
+      fetchErrorMessage: "",
+
+      esaText: "RAW TEXT",
+      esaHtml: "<span>hello world!<span>",
+      reloadDailyReport: () => { }
+    }
+    const renderResult = render(
+      <DailyReport {...props} />
+    );
+
+    fireEvent.click(renderResult.getByText("text"));
+    await waitFor(() => {
+      expect(renderResult.asFragment()).toMatchSnapshot();
+      expect(renderResult.getByText("RAW TEXT")).toBeTruthy();
+      expect(renderResult.queryByText("hello world")).toBeNull();
+    });
   });
 }); 
